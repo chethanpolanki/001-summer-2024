89bf98ad51c6fc381c1480c3d697a3148ae40a9e
Chethan Polanki <chethanpolanki@gmail.com>
2024-06-10 10:34:56 +0530

    today

---
+++ Class/0610/0610.php
@@ -0,0 +1,37 @@
+<!-- 
+
+    xhttp methods
+    get and post
+    look about it later
+
+    i needs a server for it to run
+    wamp server - the folder has to be www (if we are using it for server side)
+
+
+-->
+
+<!-- 
+
+
+    about xml is discussed
+    we can use whatever tag we want but has to be closed mandatory
+    mysql is the default go to DB when using php
+    - have to see the material uploaded by MAAM
+    
+    need to know how to create a request and display the response
+        - what is ajax and properties and examples in ppt uploaded by MAAM
+
+
+    <students>    
+        <student>
+            <name>chethan</name>
+            <age>23</age>
+            <country> India </country>
+        </student> 
+    </students>
+-->
+
+<!-- 
+    prepare marketing material in the mean time while saketh is sorting the DB
+
+-->
