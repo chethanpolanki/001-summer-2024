5c5b088f0aecaa6c4843443db3031d9ec3c49742
Chethan Polanki <chethanpolanki@gmail.com>
2024-07-02 10:28:50 +0530

    sim

--- lab/PREPLABFAT/server.js
+++ lab/PREPLABFAT/server.js
@@ -4,20 +4,22 @@ bodyparser = require("body-parser"),
 mongoose = require("mongoose");
 var cors = require('cors');
 customerModel = require("./models/customers");
+
+
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: true }));
-//app.set("view engine", "ejs");
+
 mongoose.connect('mongodb://127.0.0.1:27017/mydb');
 app.get("/", async(req, res) =>{ 
-try{
-const r = await customerModel.find({});
-res.send(r);
-//res.render("pages/results", { customers: r });
- }catch(error)
-{
-res.status(500).send({error});
-}
-})
+    try{
+    const r = await customerModel.find({});
+    res.send(r);
+    }
+
+    catch(error){
+        res.status(500).send({error});
+    }
+    })
 app.listen(3000, "localhost", function () {
-console.log("server has started");
+    console.log("server has started");
 })
